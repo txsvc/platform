5ffe53cadb078d2d903e1a26752bda6a53636df5
Michael Kuehl <hello@txs.vc>
2021-05-15 10:37:22 +0200

    merged files

--- authentication/authentication_test.go
+++ authentication/authentication_test.go
@@ -9,6 +9,26 @@ import (
 	"github.com/txsvc/platform/v2/pkg/account"
 )
 
+const (
+	scopeProductionRead  = "production:read"
+	scopeProductionWrite = "production:write"
+	scopeProductionBuild = "production:build"
+	scopeResourceRead    = "resource:read"
+	scopeResourceWrite   = "resource:write"
+)
+
+func TestScope(t *testing.T) {
+
+	scope1 := "production:read,production:write,production:build"
+
+	assert.False(t, hasScope("", ""))
+	assert.False(t, hasScope(scope1, ""))
+	assert.False(t, hasScope("", scopeResourceRead))
+
+	assert.True(t, hasScope(scope1, scopeProductionRead))
+	assert.False(t, hasScope(scope1, scopeResourceRead))
+}
+
 func TestConfirmLoginChallenge(t *testing.T) {
 	cleanup()
 	t.Cleanup(cleanup)
