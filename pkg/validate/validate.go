fba85129920949be0e649d2b78ef7f3b25444b86
Michael Kuehl <hello@txs.vc>
2021-06-04 16:12:26 +0200

    added validators

--- pkg/validate/validate.go
+++ pkg/validate/validate.go
@@ -12,3 +12,16 @@ func NotEmpty(claims ...string) bool {
 	}
 	return true
 }
+
+// IsMemberOf returns true if claim is part of the list of claims
+func IsMemberOf(claim string, claims ...string) bool {
+	if len(claims) == 0 {
+		return false
+	}
+	for _, s := range claims {
+		if s == claim {
+			return true
+		}
+	}
+	return false
+}
